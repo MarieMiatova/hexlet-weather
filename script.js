b4e47622c4991b5f53395017b436139f103aeb26
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2026-03-19 21:19:28 +0300

    title fix

--- script.js
+++ script.js
@@ -46,8 +46,6 @@ form.addEventListener("submit", async (e) => {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  msg.textContent = "Населенный пункт был найден";
-
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -60,24 +58,23 @@ form.addEventListener("submit", async (e) => {
     const li = document.createElement("li");
     li.classList.add("city");
     const markup = `
-            <h2 class="city-name" data-name="${name},${sys.country}">
-              <span>${name}</span>
-              <sup>${sys.country}</sup>
-            </h2>
-            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-            <figure>
-              <img class="city-icon" src="${icon}" alt="${
-                weather[0]["description"]
-              }">
-              <figcaption>${weather[0]["description"]}</figcaption>
-            </figure>
-          `;
-    li.innerHTML = markup;
+    <h2 class="city-name" data-name="${name},${sys.country}">
+        <span>${name}</span>
+        <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+        <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+        <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+        `;
+         li.innerHTML = markup;
     list.appendChild(li);
     msg.textContent = "";
   } catch (e) {
     console.error(e);
-  }
+    msg.textContent = "Населенный пункт не найден";
+    }
 
   form.reset();
   input.focus();
